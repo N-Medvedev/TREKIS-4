0a298f38b291bdd4475873311092166a006fb279
N. Medvedev <n.a.medvedev@gmail.com>
2025-11-24 10:11:35 +0100

    Photoabsorption merge between atomic and valence improved
    - Merger between EPDL and valence-band cross sections limit set 200 eV instead of 100 eV, which was insufficient for some materials to cross between the two curves
    - Gnuplotting style changed from png to pngcairo

--- Source_files/Output.f90
+++ Source_files/Output.f90
@@ -86,7 +86,7 @@ character(200) :: m_output_MD_displacements
 
 
 ! code version:
-character(30), parameter :: m_TREKIS_version = 'TREKIS-4 (version 01.10.2025)'
+character(30), parameter :: m_TREKIS_version = 'TREKIS-4 (version 24.11.2025)'
 
 
 ! All output file names:
